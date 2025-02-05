b9a1b9c7c7b0f3a41af9e9ec6bc60d8aa93db79b
david <davidram2303@gmail.com>
2025-02-04 23:17:13 -0600

    Test

--- modules/puestos/molinero/Molinero.ts
+++ modules/puestos/molinero/Molinero.ts
@@ -1,10 +1,25 @@
 import { Router } from "https://deno.land/x/oak@v12.6.0/mod.ts";
+import { getDBClient } from "../../../utils/db.ts";
 
 const Molinero = new Router();
 
 // POST /login para autenticar un usuario
-Molinero.get("/", async (ctx) => {
+Molinero.get("/molinero", async (ctx) => {
     ctx.response.body = "Hello World!";
     ctx.response.status = 200;
 });
+
+Molinero.post("/molinero/cocer", async (ctx) => {
+ 
+    const { name } = await ctx.request.body().value;
+    console.log('Nombre: '+name);
+    
+
+    //contestart conm un 200
+    ctx.response.status =200;
+});
+
+
+
+
 export default Molinero;
